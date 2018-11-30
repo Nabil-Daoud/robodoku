6a55cfff1dd7259d0e4faae74d3f81b983803020
Nabil-Daoud <ndaoud@athn.org>
2018-11-29 17:41:13 -0700

    Fix getSqrHas() for 2x2, and implement Phase2 functions.

--- solver/solver.go
+++ solver/solver.go
@@ -34,8 +34,8 @@ func getSqrHas(board board.Board, rowIndex int, colIndex int) []bool {
 		squareLength = 3
 	} else if boardLength == 4 {
 		squareLength = 2
-	} else if boardLength == 2 {
-		squareLength = 2
+	} else if boardLength == 2 { // this case is degenerate
+		return sqrHas
 	}
 
 	for i := 0; i < squareLength; i++ {
@@ -85,11 +85,12 @@ func solveSinglePossible(board board.Board) (bool, board.Board) {
 
 	for rowIndex, row := range board.Spots {
 		for colIndex, value := range row {
-			if value == uint8(0) {
+			if value == uint8(0) { // if unsolved
 				hasSinglePossible, val := singlePossible(rowIndex, colIndex, board)
 				if hasSinglePossible {
 					didSolve = true
 					board.Spots[rowIndex][colIndex] = val
+					// fmt.Println("SolveSinglePossible got one!")
 				}
 			}
 		}
@@ -101,7 +102,6 @@ func solveSinglePossible(board board.Board) (bool, board.Board) {
 // other values in its row, column, and square
 func Phase1(board board.Board) board.Board {
 	var didSolve, done bool
-
 	// count := 0
 	for !done {
 		// count++
@@ -109,14 +109,209 @@ func Phase1(board board.Board) board.Board {
 		didSolve, board = solveSinglePossible(board)
 		done = !didSolve
 	}
-	// for board.UpdatePossible() {
-	// 	board.SolveSinglePossible()
-	// }
 	return board
 }
 
-// Phase2 goes through the rows, columns and squares looking for and solving
-// squares that have the only possilbe for any given value
-// func Phase2(board board.Board) board.Board {
-// 	return board
-// }
+func onlyPossibleInRow(rowIndex int, board board.Board) (isThere bool, value uint8, colIndex int) {
+	var numCandidate int
+
+	// loop through all the values
+	for i, hasValue := range getRowHas(board, rowIndex) {
+		// if the row is missing a value
+		if !hasValue {
+			// loop through the spots in the row
+			for j := range board.Spots {
+				// if the spot can be the value from the first loop
+				if spotPossible(rowIndex, j, board)[i] {
+					// increment the number of candidates
+					numCandidate++
+					value = uint8(i + 1)
+					colIndex = j
+				} else {
+				}
+			}
+			if numCandidate == 1 {
+				isThere = true
+				return
+			}
+			numCandidate = 0
+		}
+	}
+	return false, 0, 0
+}
+
+func solveOnlyPossibleRow(board board.Board) (bool, board.Board) {
+	var didSolve bool
+
+	for rowIndex := range board.Spots {
+		isThere, value, colIndex := onlyPossibleInRow(rowIndex, board)
+		if isThere {
+			// fmt.Println("On board:")
+			// board.PrintBoard()
+			// fmt.Printf("Spot[%v][%v] is only in row that can be %v\n\n", rowIndex, colIndex, value)
+
+			didSolve = true
+			board.Spots[rowIndex][colIndex] = value
+
+		}
+	}
+	return didSolve, board
+}
+
+func onlyPossibleInCol(colIndex int, board board.Board) (isThere bool, value uint8, rowIndex int) {
+	var numCandidate int
+
+	// loop through all the values
+	for i, hasValue := range getColHas(board, colIndex) {
+		// if the column is missing a value
+		if !hasValue {
+			// loop through the spots in the column
+			for j := range board.Spots {
+				// if the spot can be the value from the first loop
+				if spotPossible(j, colIndex, board)[i] {
+					// increment the number of candidates
+					numCandidate++
+					value = uint8(i + 1)
+					rowIndex = j
+				}
+			}
+			if numCandidate == 1 {
+				isThere = true
+				return
+			}
+			numCandidate = 0
+		}
+	}
+	return false, 0, 0
+}
+
+func solveOnlyPossibleCol(board board.Board) (bool, board.Board) {
+	var didSolve bool
+
+	for colIndex := range board.Spots {
+		isThere, value, rowIndex := onlyPossibleInCol(colIndex, board)
+		if isThere {
+			// fmt.Println("On board:")
+			// board.PrintBoard()
+			// fmt.Printf("Spot[%v][%v] is only in column that can be %v\n\n", rowIndex, colIndex, value)
+
+			didSolve = true
+			board.Spots[rowIndex][colIndex] = value
+		}
+	}
+	return didSolve, board
+}
+
+func onlyPossibleInSqr(sqrRowIndex int, sqrColIndex int, board board.Board) (isThere bool, value uint8, rowIndex int, colIndex int) {
+	var numCandidate int
+	boardLength := len(board.Spots)
+	var squareLength int
+
+	if boardLength == 9 {
+		squareLength = 3
+	} else if boardLength == 4 {
+		squareLength = 2
+	} else if boardLength == 2 { // this case is degenerate
+		return false, 0, 0, 0
+	}
+
+	// loop through all the values
+	for i, hasValue := range getSqrHas(board, sqrRowIndex*squareLength, sqrColIndex*squareLength) {
+		// fmt.Printf("Checking value %v\n", i+1)
+
+		// if the square is missing a value
+		if !hasValue {
+			// fmt.Printf("The [%v],[%v] square does not have %v\n", sqrRowIndex, sqrColIndex, i+1)
+			// loop through the spots in the square
+			for j := 0; j < squareLength; j++ {
+				for k := 0; k < squareLength; k++ {
+					// if the spot can be the value from the first loop
+					if spotPossible(sqrRowIndex*squareLength+j, sqrColIndex*squareLength+k, board)[i] {
+						// fmt.Printf("Spot at [%v][%v] can be %v\n", sqrRowIndex*squareLength+j, sqrColIndex*squareLength+k, i+1)
+
+						// increment the number of candidates
+						numCandidate++
+						value = uint8(i + 1)
+						rowIndex = j
+					}
+				}
+			}
+			if numCandidate == 1 {
+				isThere = true
+				return
+			}
+			numCandidate = 0
+		}
+	}
+	return false, 0, 0, 0
+}
+
+func solveOnlyPossibleSqr(board board.Board) (bool, board.Board) {
+	var didSolve bool
+	var numSquares int
+	boardLength := len(board.Spots)
+
+	if boardLength == 9 {
+		numSquares = 3
+	} else if boardLength == 4 {
+		numSquares = 2
+	} else if boardLength == 2 { // this case is degenerate
+		return false, board
+	}
+
+	for sqrRowIndex := 0; sqrRowIndex < numSquares; sqrRowIndex++ {
+		for sqrColIndex := 0; sqrColIndex < numSquares; sqrColIndex++ {
+			isThere, value, rowIndex, colIndex := onlyPossibleInSqr(sqrRowIndex, sqrColIndex, board)
+			if isThere {
+				// fmt.Println("On board:")
+				// board.PrintBoard()
+				// fmt.Printf("Spot[%v][%v] is only in square that can be %v\n\n", rowIndex, colIndex, value)
+
+				didSolve = true
+				board.Spots[rowIndex][colIndex] = value
+			}
+		}
+	}
+	return didSolve, board
+}
+
+func solveOnlyPossible(board board.Board) (didSolve bool, solvedBoard board.Board) {
+	var didSolveRow, didSolveCol, didSolveSqr bool
+
+	solvedBoard = board
+
+	didSolveRow, solvedBoard = solveOnlyPossibleRow(solvedBoard)
+	didSolveCol, solvedBoard = solveOnlyPossibleCol(solvedBoard)
+	didSolveSqr, solvedBoard = solveOnlyPossibleSqr(solvedBoard)
+
+	didSolve = didSolveRow || didSolveCol || didSolveSqr
+	// fmt.Println("solveOnlyPossible got one!")
+	return
+
+	// for rowIndex, row := range board.Spots {
+	// 	for colIndex, value := range row {
+	// 		if value == uint8(0) { // if unsolved
+	// 			hasSinglePossible, val := singlePossible(rowIndex, colIndex, board)
+	// 			if hasSinglePossible {
+	// 				didSolve = true
+	// 				board.Spots[rowIndex][colIndex] = val
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+}
+
+// Phase2 goes through the rows, columns and squares (peer groups) and solve
+// if a spot is the only one of its peers that can be a given value
+func Phase2(board board.Board) board.Board {
+	var didSolve, done bool
+	// count := 0
+	for !done {
+		// count++
+		// fmt.Printf("Lap count: %v\n", count)
+		didSolve, board = solveOnlyPossible(board)
+		done = !didSolve
+	}
+	return board
+}
--- solver/solver_test.go
+++ solver/solver_test.go
@@ -4,32 +4,179 @@ import (
 	"testing"
 
 	"github.com/Nabil-Daoud/robodoku/sudoku/board"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestGetRowHas(t *testing.T) {
+	assert := assert.New(t)
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {1, 2, 3, 4}}}
+	got := getRowHas(testBoard, 0)
+	assert.Equal([]bool{true, true, false, false}, got, "getRowHas did not work.")
+}
+
+func TestGetColHas(t *testing.T) {
+	assert := assert.New(t)
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {1, 2, 0, 4}}}
+	got := getColHas(testBoard, 0)
+	assert.Equal([]bool{true, false, false, false}, got, "getColHas did not work.")
+}
+
+func TestGetSqrHas(t *testing.T) {
+	assert := assert.New(t)
+
+	testBoard := board.Board{Spots: [][]uint8{{0, 1}, {2, 1}}}
+	got := getSqrHas(testBoard, 0, 0)
+	assert.Equal([]bool{false, false}, got, "getSqrHas did not work for 2x2.")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {1, 2, 3, 4}}}
+	got = getSqrHas(testBoard, 0, 3)
+	assert.Equal([]bool{true, true, true, true}, got, "getSqrHas did not work.")
+}
+
+func TestSpotPossible(t *testing.T) {
+	assert := assert.New(t)
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {1, 2, 0, 0}}}
+	got := spotPossible(3, 2, testBoard)
+	assert.Equal([]bool{false, false, false, true}, got, "spotPossible did not work.")
+}
+
+func TestSinglePossible(t *testing.T) {
+	assert := assert.New(t)
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {1, 2, 0, 0}}}
+	gotBool, gotValue := singlePossible(3, 2, testBoard)
+	assert.Equal(true, gotBool, "singlePossible should be true.")
+	assert.Equal(uint8(4), gotValue, "singlePossible value should be 4.")
+}
+
+func TestSolveSinglePossible(t *testing.T) {
+	assert := assert.New(t)
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {0, 1, 2, 3}, {0, 2, 0, 0}}}
+	gotBool, gotBoard := solveSinglePossible(testBoard)
+	assert.Equal(true, gotBool, "solveSinglePossible should be true (on first run).")
+	expectedBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {4, 1, 2, 3}, {3, 2, 4, 1}}}
+	assert.Equal(expectedBoard, gotBoard, "Board solved incorrectly.")
+
+	gotBool, gotBoard = solveSinglePossible(gotBoard)
+	assert.Equal(false, gotBool, "solveSinglePossible should be false (on second run).")
+	expectedBoard = board.Board{Spots: [][]uint8{{0, 0, 1, 2}, {0, 0, 3, 4}, {4, 1, 2, 3}, {3, 2, 4, 1}}}
+	assert.Equal(expectedBoard, gotBoard, "Board solved incorrectly.")
+}
+
+func TestOnlyPossibleInRow(t *testing.T) {
+	assert := assert.New(t)
+
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {1, 2, 3, 4}, {2, 3, 4, 1}, {4, 1, 2, 3}}}
+	gotBool, gotValue, gotColIndex := onlyPossibleInRow(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInRow should have returned isThere = true.")
+	assert.Equal(1, int(gotValue), "onlyPossibleInRow should have returned value = 1")
+	assert.Equal(2, gotColIndex, "onlyPossibleInRow should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {2, 3, 4, 1}, {4, 1, 2, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInRow(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInRow should have returned isThere = true.")
+	assert.Equal(2, int(gotValue), "onlyPossibleInRow should have returned value = 2")
+	assert.Equal(3, gotColIndex, "onlyPossibleInRow should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {0, 3, 4, 1}, {4, 1, 0, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInRow(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInRow should have returned isThere = true.")
+	assert.Equal(3, int(gotValue), "onlyPossibleInRow should have returned value = 3")
+	assert.Equal(0, gotColIndex, "onlyPossibleInRow should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {0, 0, 4, 1}, {4, 1, 0, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInRow(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInRow should have returned isThere = true.")
+	assert.Equal(4, int(gotValue), "onlyPossibleInRow should have returned value = 3")
+	assert.Equal(1, gotColIndex, "onlyPossibleInRow should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {0, 0, 4, 1}, {0, 1, 0, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInRow(0, testBoard)
+	assert.Equal(false, gotBool, "onlyPossibleInRow should have returned isThere = true.")
+	assert.Equal(0, int(gotValue), "onlyPossibleInRow should have returned value = 3")
+	assert.Equal(0, gotColIndex, "onlyPossibleInRow should have returned colIndex = 0")
+
+}
+
+func TestOnlyPossibleInCol(t *testing.T) {
+	assert := assert.New(t)
+
+	testBoard := board.Board{Spots: [][]uint8{{0, 1, 2, 4}, {0, 2, 3, 1}, {0, 3, 4, 2}, {0, 4, 1, 3}}}
+	gotBool, gotValue, gotColIndex := onlyPossibleInCol(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInCol should have returned isThere = true.")
+	assert.Equal(1, int(gotValue), "onlyPossibleInCol should have returned value = 1")
+	assert.Equal(2, gotColIndex, "onlyPossibleInCol should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 2, 4}, {0, 2, 3, 1}, {0, 3, 4, 2}, {0, 4, 1, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInCol(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInCol should have returned isThere = true.")
+	assert.Equal(2, int(gotValue), "onlyPossibleInCol should have returned value = 2")
+	assert.Equal(3, gotColIndex, "onlyPossibleInCol should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {0, 3, 4, 1}, {4, 1, 0, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInCol(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInCol should have returned isThere = true.")
+	assert.Equal(3, int(gotValue), "onlyPossibleInCol should have returned value = 3")
+	assert.Equal(0, gotColIndex, "onlyPossibleInCol should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 4}, {0, 2, 0, 1}, {0, 3, 4, 0}, {0, 4, 1, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInCol(0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInCol should have returned isThere = true.")
+	assert.Equal(4, int(gotValue), "onlyPossibleInCol should have returned value = 4")
+	assert.Equal(1, gotColIndex, "onlyPossibleInCol should have returned colIndex = 1")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 2, 3, 4}, {0, 0, 4, 1}, {0, 1, 0, 3}}}
+	gotBool, gotValue, gotColIndex = onlyPossibleInCol(0, testBoard)
+	assert.Equal(false, gotBool, "onlyPossibleInCol should have returned isThere = false.")
+	assert.Equal(0, int(gotValue), "onlyPossibleInCol should have returned value = 0")
+	assert.Equal(0, gotColIndex, "onlyPossibleInCol should have returned colIndex = 0")
+
+}
+
+func TestOnlyPossibleInSqr(t *testing.T) {
+	assert := assert.New(t)
+
+	testBoard := board.Board{Spots: [][]uint8{{0, 0, 1, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}}}
+	gotBool, gotValue, gotRowIndex, gotColIndex := onlyPossibleInSqr(0, 0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInSqr should have returned isThere = true.")
+	assert.Equal(1, int(gotValue), "onlyPossibleInSqr should have returned value = 1")
+	assert.Equal(1, gotRowIndex, "onlyPossibleInSqr should have returned rowIndex = 1")
+	assert.Equal(0, gotColIndex, "onlyPossibleInSqr should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}}}
+	gotBool, gotValue, gotRowIndex, gotColIndex = onlyPossibleInSqr(0, 0, testBoard)
+	assert.Equal(true, gotBool, "onlyPossibleInSqr should have returned isThere = true.")
+	assert.Equal(1, int(gotValue), "onlyPossibleInSqr should have returned value = 1")
+	assert.Equal(0, gotRowIndex, "onlyPossibleInSqr should have returned rowIndex = 0")
+	assert.Equal(0, gotColIndex, "onlyPossibleInSqr should have returned colIndex = 0")
+
+	testBoard = board.Board{Spots: [][]uint8{{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}}}
+	gotBool, gotValue, gotRowIndex, gotColIndex = onlyPossibleInSqr(0, 0, testBoard)
+	assert.Equal(false, gotBool, "isThere")
+	assert.Equal(0, int(gotValue), "value")
+	assert.Equal(0, gotRowIndex, "rowIndex")
+	assert.Equal(0, gotColIndex, "colIndex")
+}
 
 func Test(t *testing.T) {
 
-	var board = board.Build([]uint8{32, 32, 32, 50, 54, 32, 55, 32, 49, 10, 54, 56, 32, 32, 55, 32, 32, 57, 32, 10, 49, 57, 32, 32, 32, 52, 53, 32, 32, 10, 56, 50, 32, 49, 32, 32, 32, 52, 32, 10, 32, 32, 52, 54, 32, 50, 57, 32, 32, 10, 32, 53, 32, 32, 32, 51, 32, 50, 56, 10, 32, 32, 57, 51, 32, 32, 32, 55, 52, 10, 32, 52, 32, 32, 53, 32, 32, 51, 54, 10, 55, 32, 51, 32, 49, 56, 32, 32, 32, 10})
-	Phase1(board)
-	// fmt.Printf("rowHas %t\n", rowHas(board, 0))
-	getRowHas(board, 0)
-	getColHas(board, 0)
-	getSqrHas(board, 0, 0)
-	singlePossible(0, 0, board)
+	testBoard, _ := board.Build([]uint8{32, 32, 32, 50, 54, 32, 55, 32, 49, 10, 54, 56, 32, 32, 55, 32, 32, 57, 32, 10, 49, 57, 32, 32, 32, 52, 53, 32, 32, 10, 56, 50, 32, 49, 32, 32, 32, 52, 32, 10, 32, 32, 52, 54, 32, 50, 57, 32, 32, 10, 32, 53, 32, 32, 32, 51, 32, 50, 56, 10, 32, 32, 57, 51, 32, 32, 32, 55, 52, 10, 32, 52, 32, 32, 53, 32, 32, 51, 54, 10, 55, 32, 51, 32, 49, 56, 32, 32, 32, 10})
+	Phase1(testBoard)
+	Phase2(testBoard)
 
-	solveSinglePossible(board)
-
-	var tests = []struct {
-		value uint8
-		want  int
-	}{
-		{1, 1},
-		{2, 2},
-	}
-	var got int
-	for _, c := range tests {
-		got = int(c.value)
-		if got != c.want {
-			t.Errorf("Cell(%v) == %v, want %v", c.value, got, c.want)
-		}
-	}
+	solveOnlyPossibleRow(testBoard)
+	solveOnlyPossibleCol(testBoard)
+	// var tests = []struct {
+	// 	value uint8
+	// 	want  int
+	// }{
+	// 	{1, 1},
+	// 	{2, 2},
+	// }
+	// var got int
+	// for _, c := range tests {
+	// 	got = int(c.value)
+	// 	if got != c.want {
+	// 		t.Errorf("Cell(%v) == %v, want %v", c.value, got, c.want)
+	// 	}
+	// }
 }
